db06c74386c967f07c8c26b7e91b32d57ae41749
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:37:02 +0300

    Сделал второе задание

---
+++ work 002/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введи первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2)
+    if (num1 > num3)
+
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(num1);
+}
+
+if (num2 >= num3)
+
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(num2);
+}
+else 
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(num3);
+}
