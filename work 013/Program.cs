085b09c72e649f99f7e31be4b0a79cf1e3f58553
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:54:34 +0300

    Сделал третью задачу второго урока

---
+++ work 013/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введи первое число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (( 1 < num) & ( num < 6))
+{
+System.Console.WriteLine("Сегодня будний день");
+}
+else
+{
+System.Console.WriteLine("Сегодня выходной");
+}
