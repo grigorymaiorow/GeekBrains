5b304726a85832a0df2ba95953a59b2bbc301040
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:40:35 +0300

    Сделал третье задание

---
+++ work 003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введи число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 2;
+
+if (result == 0)
+
+{
+    Console.Write("Число чётное");    
+}
+
+else
+
+{
+    Console.Write("Число нечётное");
+}
