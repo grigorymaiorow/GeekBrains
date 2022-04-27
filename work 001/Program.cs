60a387c4f472d3699ecc86b164cd344772536674
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:29:33 +0300

    Сделал первое домашнее задание

---
+++ work 001/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введи первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(num1);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num2);
+}
+
+if (num1 < num2)
+
+{
+    Console.Write("Большее число: ");
+    Console.WriteLine(num2);
+    Console.Write("Меньшее число: ");
+    Console.WriteLine(num1);
+}
+
+else
+
+{
+    Console.Write("Числа равны другу");
+}
