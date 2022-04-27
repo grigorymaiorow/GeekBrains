86c1900bf7f80195d3082d4c680ef351ffb44929
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:53:09 +0300

    Сделал вторую задачу второго урока

---
+++ work 012/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введи число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+while ( num > 1000)
+{
+    num = num / 10;
+}
+int res = num % 10;
+System.Console.WriteLine(res);
