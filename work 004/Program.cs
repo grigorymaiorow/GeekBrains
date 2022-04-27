df7d02d08fffbbd50bfc5eeccc9c813deea23a15
Grigoriy <g-mayorov1996@mail.ru>
2022-04-27 15:48:12 +0300

    Сделал четвёртое задание

---
+++ work 004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введи число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+while (i <= N)
+{
+    int b = i % 2;
+
+    if ( b == 0 )
+    {
+        Console.Write($"{i}, " );
+    }
+i++;
+}
+
